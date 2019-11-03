70a54f88d7bc6b5f81af5ce3ec50792f06566b30
sewas87 <vitalliy.sevastyanov@gmail.com>
2019-11-03 19:30:52 +0200

    Created login and register

---
+++ mob/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login";
+import {HomeComponent} from "./home";
+import {AuthGuard} from "./_helpers";
+import {RegisterComponent} from "./register";
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
---
+++ mob/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app-routing.module';
+import { LoginComponent } from "./login";
+import { HomeComponent } from "./home";
+import { RegisterComponent } from "./register";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ErrorInterceptor, JwtInterceptor} from "./_helpers";
+import { AlertComponent } from './_components';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    AlertComponent
+
+  ],
+  entryComponents: [],
+  imports: [
+      BrowserModule,
+    IonicModule.forRoot(),
+    appRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
